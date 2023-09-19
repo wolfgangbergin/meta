07b9daeca4d9fba07c20c9007ccbcc05ad064de2
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-09-19 14:05:04 -0400

    temp

--- app.ts
+++ app.ts
@@ -3,6 +3,5 @@ export { wolfgang } from './wolfgang.js';
 export { temp1 } from './temp1.js';
 
 
-d(Symbol)
 
 
--- temp1.ts
+++ temp1.ts
@@ -1,38 +1,32 @@
 /////////////////////////////////////////////// Library
-const wolfId = Symbol('Symbol.wolfId')
-
-
-
+const wolfId = Symbol('Symbol.wolfId');
 
 const person = {
-    id: 'p1',
-    [wolfId]: 'p1',
-    [Symbol.toStringTag]: 'Person',
-   
-}
-
+  id: 'p1',
+  [wolfId]: 'p1',
+  [Symbol.toStringTag]: 'Person',
+};
 
 ///////////////////////////////////////////////app
 
+person[wolfId] = 'wolf';
 
-person[wolfId]= 'wolf'
-
-
-person[Symbol.toStringTag] = 'kim'
-
-
-person
-
+person[Symbol.toStringTag] = 'kim';
 
 const company = {
-    employees: ['bob', 'kim', 'joe'],
-    next() {
-    return {vale: this.employees[0], done: false}
+  curEmploee: 0,
+  employees: ['bob', 'kim', 'joe'],
+  next() {
+    if (this.curEmploee >= this.employees.length) {
+      return { value: this.curEmploee, done: true };
     }
 
-}
+    const returnValue = { value: this.employees[this.curEmploee], done: false };
+    this.curEmploee++;
+    return returnValue;
+  },
+};
 
+let temp1 = 'temp1';
 
-let temp1 = 'temp1'
-
-export {temp1, person}
+export { temp1, person };
